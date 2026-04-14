57eddd6e26a75151354bef76cfeaa67683e15bcf
KoreanBadboy <giang_hoang@hotmail.com>
2026-04-14 14:33:08 +0200

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const hej = "hej";
+
+function hejsan()
+{
+    const collection = document.getElementById("myHeader");
+    collection.textContent = "The Cat Store!";
+}
+
+// let count = 0;
+
+// function uppdateraRaknare() {
+//     count++;
+//     document.getElementById("countUpText").textContent = count;
+// }
+
+// setInterval(uppdateraRaknare, 1000);
+
